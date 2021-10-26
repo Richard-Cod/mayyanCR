542dc7a53758850c94112494ab7ccc22f075a731
Richard Bathiebo <richard.bathiebo.7@gmail.com>
2021-10-26 15:17:10 +0200

    s

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import { MenuIcon } from '@heroicons/react/solid'
+
+import Image from 'next/image'
+
 
 function Navbar() {
     return (
@@ -12,10 +16,14 @@ function Navbar() {
 
                 <div className="ml-auto">
 
-                    <ul className="flex">
+                    <ul className="flex hidden md:flex">
                         <li className="text-md mr-8 cursor-pointer ">Selected Coverage</li>
                         <li className="text-md mr-8 cursor-pointer ">Contact</li>
                     </ul>
+
+                    <MenuIcon className="h-10 w-10 md:hidden"  />
+
+
 
                 </div>
 
