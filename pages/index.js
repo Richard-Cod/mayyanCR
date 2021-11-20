b18c52ba7e4ea64bebe29a16d875a5fabb2ba2a0
Richard Bathiebo <richard.bathiebo.7@gmail.com>
2021-11-20 11:17:50 +0100

    e

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className="">
       <Head>
-        <title>Create Next App</title>
+        <title>Create  App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
